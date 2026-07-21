168d9486f941b5113fc46580e1f070c8548176b9
Fatemeh Salehtabar <f.salehtabar@gmail.com>
2026-07-21 20:39:03 +0330

    feat: add user input and type conversion exercise

---
+++ user_input_and_type_conversion/user_input_and_type_conversion.py
@@ -0,0 +1,7 @@
+user_name= input("Enter your name:")
+hi_user= f"Hello {user_name}!"
+user_age = input("Enter your age?")
+int_user_age = int(user_age)
+show_user_age = f"You are {int_user_age} years old"
+print(hi_user)
+print(show_user_age)
