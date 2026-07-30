337a3a2e2943efc0376e3e7d0f6a0b7df99a28bb
Fatemeh Salehtabar <f.salehtabar@gmail.com>
2026-07-30 18:50:36 +0330

    "feat: convert input string to reversed list of integers"

---
+++ append_number_to_list/append_number_list.py
@@ -0,0 +1,19 @@
+user_number = input("Enter a number:")
+user_number_list = list(user_number)
+print(user_number_list)
+new_list = []
+for number in user_number_list:
+    int_user_number = int(number)
+    new_list.append(int_user_number)
+
+print(new_list[::-1])
+
+     
+   
+    
+    
+
+
+
+        
+    
