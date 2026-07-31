12af1bbe168ac68173495f926596346cf840fdc4
Fatemeh Salehtabar <f.salehtabar@gmail.com>
2026-07-31 17:12:53 +0330

    feat: require mixed-case password

--- password_sterength/password_sterength.py
+++ password_sterength/password_sterength.py
@@ -24,12 +24,20 @@ if len(password) > 8:
     score+=1
 else:
     erorr_list.append("length of password less than 8")
+    
 if contain_alpha(password) and contain_special_charecter(password):
     score+=1
 else:
     erorr_list.append("passwod dosent have english char and (@,!, $) ")
+    
 if password != user_name:
     score+=1
 else:
     erorr_list.append("password simillar to the username")
+
+if password != password.upper() and password != password.lower():
+    score+=1
+else:
+    erorr_list.append("Password must contain both uppercase or lowercase letters.")
+
  
