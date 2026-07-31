e7a789f0d8f5d7e7a49bd64c604a507273b057f7
Fatemeh Salehtabar <f.salehtabar@gmail.com>
2026-07-31 17:05:55 +0330

    feat: add basic password validation

---
+++ password_sterength/password_sterength.py
@@ -0,0 +1,31 @@
+user_name :str =input("Enter username: ")
+password :str =input("Enter password: ")
+PASWORD_STERENGTH :int= 7
+score:int= 0
+erorr_list = []
+
+def contain_alpha(password:str):
+    for char in password:
+        if char.isalpha():
+            return True
+    return False
+
+def contain_special_charecter(password: str):
+    for char in password:
+        if char == "!" or char == "@" or char == "$":
+            return True
+    return False            
+            
+    
+
+
+
+if len(password) > 8:
+    score+=1
+else:
+    erorr_list.append("length of password less than 8")
+if contain_alpha(password) and contain_special_charecter(password):
+    score+=1
+else:
+    erorr_list.append("passwod dosent have english char and (@,!, $) ")
+ 
